96e84cf30bbbef68b77cfb606b3af0fc9ea61f87
aloric97 <leandro.tombetta87@gmail.com>
2023-05-22 20:00:34 -0300

    initial project

---
+++ src/test/java/com/API/Disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.Disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
